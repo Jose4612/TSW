562f57fdbfeae84f4790d95d7dd9163406770bb2
Unknown <jdvazquez@esei.uvigo.es>
2018-10-16 12:51:13 +0200

    TSW

---
+++ tsw-mvc-master/application/views/index.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>Pagina principal</title>
+	<link rel="stylesheet" href="./index_style.css"/>
+</head>
+<body>
+	<header>
+		<span id="user">Usuario:User123 </span>
+		<span id="logout"><button type="button" name="Log out"></button></span>
+    <span id="login"><button type="button" name="Log in"></button></span>
+		<span id="signin"><button type="button" name="Sign in"></button></span>
+	</header>
+	<article id="maincontent">MAIN Content<br>This is the main content...</article>
+	<nav id="mainnavigation">LINKS</nav>
+	<aside id="news">NEWS</aside>
+	<footer>FOOTER</footer>
+</body>
+</html>
